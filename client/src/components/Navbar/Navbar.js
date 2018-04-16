da2af51713787a8f1a46f2ccfd39b60ddfd8613d
Server <november14giel@gmail.com>
2018-04-16 00:13:18 -0400

    delete fhv2

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -42,11 +42,6 @@ const Navbar = props => {
               </a>
             </li>
             <li>
-              <a href="https://fh-v2.herokuapp.com/" className="listItems">
-                {/* <Link to="/discover">RESTful API</Link> */}Fh-V2
-              </a>
-            </li>
-            <li>
               <a href="https://fast-depths-29670.herokuapp.com/" className="listItems">
                 {/* <Link to="/discover">RESTful API</Link> */}Reading List
               </a>
